3f366c3df4d668bb841d8a458cb7e89e653672db
James Gawley <jamesgawley@Jamess-MacBook-Air-6.local>
2021-06-15 21:41:14 +0200

    Erreur typographique

--- scripts/5-generer-tsv-avec-gold-et-predictions.pl
+++ scripts/5-generer-tsv-avec-gold-et-predictions.pl
@@ -161,7 +161,7 @@ foreach my $i (0..$#gold_files) {
 
     open my $out, ">:utf8", $outfile or die $!;
     print $out "TOKEN\tPREDICTION\tGOLD\tVALIDITY\n";
-	chomp @gold;
+	chomp @gold_tags;
 
     print scalar(@words) . "\n";
     print scalar(@auto_tags) . "\n";
